bdbd7c120f8dad2dbe940dbe21af736c45a92d22
lostinacrowd2 <114860527+lostinacrowd2@users.noreply.github.com>
2026-04-09 15:46:36 -0400

    Implement service worker event listeners for caching

--- sw.js
+++ sw.js
@@ -7,3 +7,21 @@ const ASSETS = [
   "/daily-goal-tracker/icon-192.png",
   "/daily-goal-tracker/icon-512.png"
 ];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
