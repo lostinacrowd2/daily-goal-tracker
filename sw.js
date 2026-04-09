23b442d9442c51d1fd452a54272656a9d10490d2
lostinacrowd2 <114860527+lostinacrowd2@users.noreply.github.com>
2026-04-09 15:43:45 -0400

    Update service worker cache and assets
    Updated cache version and added new asset icons.

--- sw.js
+++ sw.js
@@ -1,20 +1,9 @@
-const CACHE = "dgt-v2";
-const ASSETS = ["/daily-goal-tracker/", "/daily-goal-tracker/index.html", "/daily-goal-tracker/manifest.json", "/daily-goal-tracker/sw.js", "/daily-goal-tracker/icon.svg"];
-
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+const CACHE = "dgt-v3";
+const ASSETS = [
+  "/daily-goal-tracker/",
+  "/daily-goal-tracker/index.html",
+  "/daily-goal-tracker/manifest.json",
+  "/daily-goal-tracker/sw.js",
+  "/daily-goal-tracker/icon-192.png",
+  "/daily-goal-tracker/icon-512.png"
+];
